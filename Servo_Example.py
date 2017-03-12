aab36ce8c7dc9f88ea68aa11d99fcb83c1182c01
Vuong Ly <imvuong@gmail.com>
2017-03-12 17:12:25 -0400

    add banner text

--- Servo_Example.py
+++ Servo_Example.py
@@ -11,6 +11,7 @@ import sys, termios, tty, os, time
 
 import argparse
 import math
+from debounce import debounce
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
@@ -28,74 +29,91 @@ pwm = PWM(0x40)
 # Note if you'd like more debug output you can instead run:
 #pwm = PWM(0x40, debug=True)
 
-servoMin = 150  # Min pulse length out of 4096
-servoMax = 600  # Max pulse length out of 4096
-
-neutral = 307
-neutral_ms = 1.5
-
+pwm_frequency = 50              # Set desired frequency (Hz)
+pwm_single_pulse_ms = 20        # Set single pulse period (ms)
+max_pulse_length = 4096         # Max pulse length (ms)
+neutral_ms = 1.5                # Servo neutral pulse width (ms)
+neutral = round(max_pulse_length / pwm_single_pulse_ms * neutral_ms)  # Neutral pulse length (ms)
+clockwise_max_ms = 1.7          # Continuous servo clockwise rotation max pulse width (ms)
+counter_clockwise_max_ms = 1.3  # Continuous servo counter clockwise rotation max pulse width (ms)
+clockwise_pulse = round(max_pulse_length / pwm_single_pulse_ms * clockwise_max_ms)
+counter_clockwise_pulse = round(max_pulse_length / pwm_single_pulse_ms * counter_clockwise_max_ms)
 
 def stop():
-    pwm.setPWM(FL_WHEEL, 0, neutral)
-    pwm.setPWM(BL_WHEEL, 0, neutral)
-    pwm.setPWM(FR_WHEEL, 0, neutral)
-    pwm.setPWM(BR_WHEEL, 0, neutral)
+    set_left_wheels(neutral)
+    set_right_wheels(neutral)
+    client.send_message("/robot/left", neutral_ms)
+    client.send_message("/robot/right", neutral_ms)
+    client.send_message("/robot/banner", "HALT!!!")
 
 def forward():
-    pwm.setPWM(FL_WHEEL, 0, 410)
-    pwm.setPWM(BL_WHEEL, 0, 410)
-    pwm.setPWM(FR_WHEEL, 0, 205)
-    pwm.setPWM(BR_WHEEL, 0, 205)
+    set_left_wheels(clockwise_pulse)
+    set_right_wheels(counter_clockwise_pulse)
+    client.send_message("/robot/left", clockwise_max_ms)
+    client.send_message("/robot/right", counter_clockwise_max_ms)
+    client.send_message("/robot/banner", "Full speed ahead!!!")
 
 def backward():
-    pwm.setPWM(FL_WHEEL, 0, 205)
-    pwm.setPWM(BL_WHEEL, 0, 205)
-    pwm.setPWM(FR_WHEEL, 0, 410)
-    pwm.setPWM(BR_WHEEL, 0, 410)
+    set_left_wheels(counter_clockwise_pulse)
+    set_right_wheels(clockwise_pulse)
+    client.send_message("/robot/left", counter_clockwise_max_ms)
+    client.send_message("/robot/right", clockwise_max_ms)
+    client.send_message("/robot/banner", "Watch out behind me!!!")
 
 def turn_left():
-    pwm.setPWM(FL_WHEEL, 0, 205)
-    pwm.setPWM(BL_WHEEL, 0, 205)
-    pwm.setPWM(FR_WHEEL, 0, 205)
-    pwm.setPWM(BR_WHEEL, 0, 205)
+    set_left_wheels(counter_clockwise_pulse)
+    set_right_wheels(counter_clockwise_pulse)
+    client.send_message("/robot/left", counter_clockwise_max_ms)
+    client.send_message("/robot/right", counter_clockwise_max_ms)
+    client.send_message("/robot/banner", "Making a left!!!")
 
 def turn_right():
-    pwm.setPWM(FL_WHEEL, 0, 410)
-    pwm.setPWM(BL_WHEEL, 0, 410)
-    pwm.setPWM(FR_WHEEL, 0, 410)
-    pwm.setPWM(BR_WHEEL, 0, 410)
+    set_left_wheels(clockwise_pulse)
+    set_right_wheels(clockwise_pulse)
+    client.send_message("/robot/left", clockwise_max_ms)
+    client.send_message("/robot/right", clockwise_max_ms)
+    client.send_message("/robot/banner", "Making a right!!!")
 
-
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+def set_left_wheels(pulse):
+    pwm.setPWM(FL_WHEEL, 0, pulse)
+    pwm.setPWM(BL_WHEEL, 0, pulse)
+    
+def set_right_wheels(pulse):
+    pwm.setPWM(FR_WHEEL, 0, pulse)
+    pwm.setPWM(BR_WHEEL, 0, pulse)
 
 def ping_handler(unused_addr, args):
     print("[{0}]".format(args[0]))
 
+@debounce(0.1)
 def left_handler(unused_addr, args, value):
     print("[{0}] ~ {1}".format(args[0], value))
+    pulse = round(max_pulse_length / pwm_single_pulse_ms * value)
+    set_left_wheels(pulse)
+    client.send_message("/robot/banner", " ")
 
+@debounce(0.1)
 def right_handler(unused_addr, args, value):
     print("[{0}] ~ {1}".format(args[0], value))
+    pulse = round(max_pulse_length / pwm_single_pulse_ms * value)
+    set_right_wheels(pulse)
+    client.send_message("/robot/banner", " ")
 
-def stop_handler(unused_addr, args, value):
+def push_button_handler(unused_addr, args, value):
     print("[{0}] ~ {1}".format(args[0], value))
-    client.send_message("/robot/left", neutral_ms)
-    client.send_message("/robot/right", neutral_ms)
+    if (args[0] == "stop"):
+        stop()
+    elif (args[0] == "turn_left"):
+        turn_left()
+    elif (args[0] == "turn_right"):
+        turn_right()
+    elif (args[0] == "forward"):
+        forward()
+    elif (args[0] == "backward"):
+        backward()
 
 
-pwm.setPWMFreq(50)                        # Set frequency to 60 Hz
-
-# Set to stop
-stop()
+pwm.setPWMFreq(pwm_frequency)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--server", default="192.168.1.17", help="The server ip to listen on")
@@ -105,13 +123,21 @@ parser.add_argument("--client-port", type=int, default=5006, help="The client po
 args = parser.parse_args()
 
 dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/ping", ping_handler, "Ping")
-dispatcher.map("/robot/left", left_handler, "Left")
-dispatcher.map("/robot/right", right_handler, "Right")
-dispatcher.map("/robot/stop", stop_handler, "Stop")
+dispatcher.map("/ping", ping_handler, "ping")
+dispatcher.map("/robot/left", left_handler, "left")
+dispatcher.map("/robot/right", right_handler, "right")
+dispatcher.map("/robot/stop", push_button_handler, "stop")
+dispatcher.map("/robot/forward", push_button_handler, "forward")
+dispatcher.map("/robot/backward", push_button_handler, "backward")
+dispatcher.map("/robot/turn_left", push_button_handler, "turn_left")
+dispatcher.map("/robot/turn_right", push_button_handler, "turn_right")
 
 server = osc_server.ThreadingOSCUDPServer((args.server, args.server_port), dispatcher)
 client = udp_client.SimpleUDPClient(args.client, args.client_port)
+
+# Set to stop
+stop()
+
 print("Serving on {}".format(server.server_address))
 server.serve_forever()
 
